1989c3ca84f605381b0e61efbc515378d8bb5a2c
Delapouite <delapouite@gmail.com>
2016-12-14 19:16:33 +0100

    feat: add formatter + basic docs

---
+++ README.md
@@ -0,0 +1,33 @@
+# ESLint formatter for kakoune
+
+[ESLint](http://eslint.org) formatter that follows [kakoune](https://github.com/mawww/kakoune/blob/master/rc/base/lint.kak) format:
+
+```
+{filename}:{line}:{column}: {kind}: {message}
+```
+
+## Install
+
+```sh
+$ npm install --save-dev eslint-formatter-kakoune
+```
+
+## Usage
+
+### ESLint CLI
+
+```
+$ eslint --format=node_modules/eslint-formatter-kakoune file.js
+```
+
+In your `kakrc`:
+
+```
+hook global WinSetOption filetype=javascript %{
+  set buffer lintcmd 'eslint --format=node_modules/eslint-formatter-kakoune'
+  lint-enable
+  lint
+}
+```
+
+You may add more hooks to trigger the lint command on save.
---
+++ index.js
@@ -0,0 +1,59 @@
+/**
+ * @fileoverview kakoune formatter.
+ * @author delapouite
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Helper Functions
+//------------------------------------------------------------------------------
+
+/**
+ * Returns a canonical error level string based upon the error message passed in.
+ * @param {Object} message Individual error message provided by eslint
+ * @returns {string} Error level string
+ */
+function getMessageType(message) {
+    if (message.fatal || message.severity === 2) {
+        return "Error";
+    } else {
+        return "Warning";
+    }
+}
+
+
+//------------------------------------------------------------------------------
+// Public Interface
+//------------------------------------------------------------------------------
+
+module.exports = function(results) {
+
+    let output = "",
+        total = 0;
+
+    results.forEach(result => {
+
+        const messages = result.messages;
+
+        total += messages.length;
+
+        messages.forEach(message => {
+
+            output += `${result.filePath}:`;
+            output += `${message.line || 0}:`;
+            output += `${message.column || 0}:`;
+            output += ` ${getMessageType(message)}:`;
+            output += ` ${message.message} (${message.ruleId}) `;
+            output += "\n";
+
+        });
+
+    });
+
+    if (total > 0) {
+        output += `\n${total} problem${total !== 1 ? "s" : ""}`;
+    }
+
+    return output;
+};
+
